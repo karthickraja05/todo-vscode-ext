9165b0311e498127e9105bd645e1f15e8af53d6c
Karthickraja <karthickraja.gm@gmail.com>
2024-01-30 22:15:35 +0530

    web view test

--- extension.js
+++ extension.js
@@ -1,36 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+const cats = {
+	'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+	'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+  };
+
 function activate(context) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('catCoding.start', () => {
+			// Create and show a new webview
+			const panel = vscode.window.createWebviewPanel(
+				'catCoding', // Identifies the type of the webview. Used internally
+				'My Todo Page', // Title of the panel displayed to the user
+				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+				{} // Webview options. More on these later.
+			);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "todo-vscode-ext" is now active!');
+			let iteration = 0;
+			const updateWebview = () => {
+				const cat = iteration++ % 2 ? 'Compiling Cat' : 'Coding Cat';
+				panel.title = cat;
+				panel.webview.html = getWebviewContent(cat);
+			};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('todo-vscode-ext.todo_app', function () {
-		// The code you place here will be executed every time your command is executed
+			// Set initial content
+			updateWebview();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Todo App Successfully init.');
-	});
-
-	context.subscriptions.push(disposable);
+			// And schedule updates to the content every second
+			setInterval(updateWebview, 1000);
+		})
+	);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function getWebviewContent(cat) {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="${cats[cat]}" width="300" />
+  </body>
+  </html>`;
+  }
 
 module.exports = {
-	activate,
-	deactivate
-}
+	activate
+};
